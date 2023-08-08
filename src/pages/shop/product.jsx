e9455fdeda1b2ff3c4f90b071d8772ba841c5976
Lucian Eugenio <lucian.aidan@gmail.com>
2023-08-07 21:00:14 -0400

    updating shop layout & modal

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "../../context/shop-context";
+import "./shop.css";
 
-export const Product = (props) => {
+export const ProductItem = (props) => {
+  // Rename to ProductItem
   const { id, productName, price, productImage } = props.data;
-  const { addToCart, cartItems } = React.useContext(ShopContext);
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   const cartItemCount = cartItems[id];
 
   return (
-    <div className="product" onClick={() => props.onOpenModal(props.data)}>
-      <img src={productImage} alt={productName} />
+    <div className="product">
+      <img src={productImage} alt={productName} onClick={toggleModal} />
       <div className="description">
         <p>
           <b>{productName}</b>
         </p>
-        <p>${price}</p>
+        <p> ${price}</p>
       </div>
       <button className="addToCartBttn" onClick={() => addToCart(id)}>
         Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
       </button>
+      {isModalOpen && (
+        <div className="overlay" onClick={toggleModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <img src={productImage} alt={productName} />
+            <div className="description">
+              <p>
+                <b>{productName}</b>
+              </p>
+              <p> ${price}</p>
+            </div>
+            <button className="addToCartBttn" onClick={() => addToCart(id)}>
+              Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,41 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { PRODUCTS } from "../../products";
-import { Product } from "./product";
+import { ProductItem } from "./product"; // Rename to ProductItem
 import "./shop.css";
-import Modal from "react-modal";
 
 export const Shop = () => {
-  const [selectedProduct, setSelectedProduct] = useState(null);
-
-  const openModal = (product) => {
-    setSelectedProduct(product);
-  };
-
-  const closeModal = () => {
-    setSelectedProduct(null);
-  };
-
   return (
     <div className="shop">
-      {PRODUCTS.map((product) => (
-        <Product key={product.id} data={product} onOpenModal={openModal} />
-      ))}
+      <div className="shopTitle"></div>
 
-    <div className="wrapper"> 
-      <Modal isOpen={selectedProduct !== null} onRequestClose={closeModal}>
-        {selectedProduct && (
-          <div className="modal-content">
-            <h2>{selectedProduct.productName}</h2>
-            <img
-              src={selectedProduct.productImage}
-              alt={selectedProduct.productName}
-            />
-            <p>${selectedProduct.price}</p>
-            <button onClick={closeModal}>X</button>
-          </div>
-        )}
-        </Modal>
-        </div>
+      <div className="products-grid">
+        {PRODUCTS.map((product) => (
+          <ProductItem key={product.id} data={product} />
+        ))}
+      </div>
     </div>
   );
 };
