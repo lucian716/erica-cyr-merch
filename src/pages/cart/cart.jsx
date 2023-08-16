17ad59d1436c205319b8709b1d4a8fb252ead580
Lucian Eugenio <lucian.aidan@gmail.com>
2023-08-16 18:31:34 -0400

    progress on paypal functionality

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ShopContext } from "../../context/shop-context";
 import { PRODUCTS } from "../../products";
 import { CartItem } from "./cart-item";
@@ -11,25 +11,34 @@ export const Cart = () => {
   const totalAmount = getTotalCartAmount();
   const navigate = useNavigate();
 
-  const PayPalButton = () => {
-    return (
-      <PayPalButtons
-        createOrder={(data, actions) => {
-          // Function to create the order on PayPal
-        }}
-        onApprove={(data, actions) => {
-          // Function to handle the successful payment
-        }}
-        onError={(error) => {
-          // Function to handle payment errors
-        }}
-      />
-    );
+  const handleSuccessfulPayment = async (order) => {
+    try {
+      // Here you can make a server request to update the order status in your system
+      // You might want to send the `order` object to your server along with other necessary data
+      const response = await fetch("/api/updateOrderStatus", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(order),
+      });
+
+      if (response.ok) {
+        // Payment was successful and order status was updated
+        console.log("Payment was successful and order status updated");
+        checkout(); // Clear the cart after successful payment
+      } else {
+        // Handle error if updating order status failed
+        console.error("Failed to update order status");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
   };
 
   return (
     <div className="cart">
-      {totalAmount > 0 ? ( 
+      {totalAmount > 0 ? (
         <div>
           <h1>Your Cart Items</h1>
           <div className="cart-items">
@@ -43,10 +52,32 @@ export const Cart = () => {
           <div className="subtotal-container">
             <div className="checkout">
               <p>Subtotal: ${totalAmount}</p>
-               <button onClick={() => navigate("/")} className="continue-shopping-button">
+              <button
+                onClick={() => navigate("/")}
+                className="continue-shopping-button"
+              >
                 Continue Shopping
               </button>
-              <PayPalButton />
+              <PayPalButtons
+                createOrder={(data, actions) => {
+                  return actions.order.create({
+                    purchase_units: [
+                      {
+                        amount: {
+                          value: totalAmount.toFixed(2), // Total amount to charge
+                        },
+                      },
+                    ],
+                  });
+                }}
+                onApprove={async (data, actions) => {
+                  const order = await actions.order.capture();
+                  handleSuccessfulPayment(order); // Call the custom handler
+                }}
+                onError={(error) => {
+                  console.error("An error occurred during payment:", error);
+                }}
+              />
             </div>
           </div>
         </div>
